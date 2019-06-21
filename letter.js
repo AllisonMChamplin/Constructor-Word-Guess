e65966ed6232026fdeeb113f7d0c77f62510d26d
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-06-21 15:35:12 -0500

    Wrote Letter constructor function in letter.js

---
+++ letter.js
@@ -0,0 +1,31 @@
+var fs = require("fs");
+
+function Letter (characterString) {
+    this.characterString = characterString;
+    this.placeHolder = "_";
+    this.guessStatus = false;
+    this.checkGuessStatus = function () {
+        if (this.guessStatus === true) {
+            return this.characterString;
+        } else {
+            return this.placeHolder;
+        }
+    }
+    this.checkCharacter = function (characterGuess) {
+        if (characterGuess === this.characterString) {
+            this.guessStatus = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// console.log("Hi");
+// var letterOne = new Letter("c");
+// console.log("letterOne: ", letterOne);
+// console.log("checkGuessStatus of letterOne upon construction: ", letterOne.checkGuessStatus());
+// console.log("Running checkCharacter on c: ", letterOne.checkCharacter("c"));
+// console.log("checkGuessStatus of letterOne now: ", letterOne.checkGuessStatus());
+
+
